fc28ea73ef46b759125d47786227798a273d0140
jmac522 <familyguy5115@gmail.com>
2018-10-11 20:38:17 +0000

    Further implamentation of getting legal moves in bishop class, TODO: implament general method to use for all four directions

--- src/Bishop.java
+++ src/Bishop.java
@@ -20,101 +20,123 @@ public class Bishop extends Piece{
     	// based on its current position 
         final List<int> potentialMoves = new ArrayList<>();
         
+        // ArrayList to hold found legal moves among potential bishop moves
+        final List<Move> legalMoves = new ArrayList<>();
+        
         
         // REFACTOR THESE INTO METHODS
+        // CONSIDER directly adding to legal moves
         //Start by examining up-right movement by incramenting by 7 until reaching 
         // column H 
         int upRightTracker = this.locationOnBoard;
-        // if the current position is not on column H
+        boolean hitPiece = false;
+        // if the current position is not on column H and we havent hit an obstructing piece
         // add the upper right square from current position to 
         // potential moves list 
-        if (!GameUtilities.COLUMN_H(upRightTracker)) {
+        if (!GameUtilities.COLUMN_H(upRightTracker) && !hitPiece) {
         	upRightTracker -= 7; // set tracker to square to upper right
-        	potentialMoves.add(upRightTracker); // add square to potential moves
+        	 // get the BoardSquare that is at the current potential move location
+            final BoardSquare potentialMoveSquare = board.getSquare(upRightTracker);
+            // if the square is not occupied we can add the square as a potential location
+            if (!potentialMoveSquare.isOccupied()) {
+            	// if the square is not occupied
+                potentialMoves.add(upRightTracker); // stubbed move object, expand later
+            } else {
+            	// if the square IS occupied check the side of the piece
+                final Piece pieceOnSquare = potentialMoveSquare.getPiece();
+                final Side pieceColor = pieceOnSquare.getColor();
+                if (this.color != pieceColor) {
+                	// if it is an opponents piece, add to list and mark hit piece true
+                	potentialMoves.add(upRightTracker);
+                	hitPiece = true;
+                } else {
+                	hitPiece = true;
+                }
+            }
         }
         
         // Examining bottom right movement, using same process
         int bottomRightTracker = this.locationOnBoard;
-        if (!GameUtilities.COLUMN_H(bottomRightTracker)) {
+        hitPiece = false;
+        if (!GameUtilities.COLUMN_H(bottomRightTracker) && !hitPiece) {
         	bottomRightTracker += 9;
-        	potentialMoves.add(bottomRightTracker);
-        }
-       
-        
-        
-        
-        // integer to keep track of the location we're currently examining
-        int currentPotentialMove;
-
-        // ArrayList to hold found legal moves among potential knight moves
-        final List<Move> legalMoves = new ArrayList<>();
-
-        // loop through each potential move to determine if it is a legal move
-        for (int potentialMoveOffset : POTENTIAL_MOVES) {
-            // current board position to check
-            currentPotentialMove = this.locationOnBoard + potentialMoveOffset;
-
-            // If the currentPotentialMove is within the valid range of the 0-63 board, and the offset does not fail
-            // any of the Column Edge case exceptions
-            if (GameUtilities.isValidBoardLocation(currentPotentialMove)       &&
-               (!isFirstColumnException(locationOnBoard, potentialMoveOffset)) &&
-               (!isLastColumnException(locationOnBoard, potentialMoveOffset))  &&
-               (!isColumnBException(locationOnBoard, potentialMoveOffset))      &&
-               (!isColumnGException(locationOnBoard, potentialMoveOffset))){
-                // get the BoardSquare that is at the current potential move location
-                final BoardSquare potentialMoveSquare = board.getSquare(currentPotentialMove);
-
-                if (!potentialMoveSquare.isOccupied()) {
-                    legalMoves.add(new Move()); // stubbed move object, expand later
+        	final BoardSquare potentialMoveSquare = board.getSquare(bottomRightTracker);
+            // if the square is not occupied we can add the square as a potential location
+            if (!potentialMoveSquare.isOccupied()) {
+            	// if the square is not occupied
+                potentialMoves.add(bottomRightTracker); // stubbed move object, expand later
+            } else {
+            	// if the square IS occupied check the side of the piece
+                final Piece pieceOnSquare = potentialMoveSquare.getPiece();
+                final Side pieceColor = pieceOnSquare.getColor();
+                if (this.color != pieceColor) {
+                	// if it is an opponents piece, add to list and mark hit piece true
+                	potentialMoves.add(bottomRightTracker);
+                	hitPiece = true;
                 } else {
-                    // if the potential square IS occupied, get the piece that is currently on that square and its
-                    // color
-                    final Piece pieceOnSquare = potentialMoveSquare.getPiece();
-                    final Side pieceColor = pieceOnSquare.getColor();
-
-
-                    // If the piece on the potential move square is not the same color as the knight, add this
-                    // square as a potential legal move
-                    if (this.color != pieceColor) {
-                        legalMoves.add(new Move()); // stubbed move object, expand later
-                    }
+                	hitPiece = true;
                 }
             }
-
+        }
+       
+       
+        // Examining bottom left movement, using same process
+        int bottomLeftTracker = this.locationOnBoard;
+        hitPiece = false;
+        if (!GameUtilities.COLUMN_A(bottomLeftTracker) && !hitPiece) {
+        	bottomRightTracker += 7;
+        	final BoardSquare potentialMoveSquare = board.getSquare(bottomLeftTracker);
+            // if the square is not occupied we can add the square as a potential location
+            if (!potentialMoveSquare.isOccupied()) {
+            	// if the square is not occupied
+                potentialMoves.add(bottomLeftTracker); // stubbed move object, expand later
+            } else {
+            	// if the square IS occupied check the side of the piece
+                final Piece pieceOnSquare = potentialMoveSquare.getPiece();
+                final Side pieceColor = pieceOnSquare.getColor();
+                if (this.color != pieceColor) {
+                	// if it is an opponents piece, add to list and mark hit piece true
+                	potentialMoves.add(bottomLeftTracker);
+                	hitPiece = true;
+                } else {
+                	hitPiece = true;
+                }
+            }
+        }
+        
+        int upLeftTracker = this.locationOnBoard;
+        hitPiece = false;
+        // check up left moves
+        if (!GameUtilities.COLUMN_A(upLeftTracker) && !hitPiece) {
+        	upLeftTracker -= 9; // set tracker to square to upper right
+        	 // get the BoardSquare that is at the current potential move location
+            final BoardSquare potentialMoveSquare = board.getSquare(upLeftTracker);
+            // if the square is not occupied we can add the square as a potential location
+            if (!potentialMoveSquare.isOccupied()) {
+            	// if the square is not occupied
+                potentialMoves.add(upLeftTracker);
+                // add to legal moves as passive move 
+                // legalMoves.add(new Move());
+            } else {
+            	// if the square IS occupied check the side of the piece
+                final Piece pieceOnSquare = potentialMoveSquare.getPiece();
+                final Side pieceColor = pieceOnSquare.getColor();
+                if (this.color != pieceColor) {
+                	// if it is an opponents piece, add to list and mark hit piece true
+                	potentialMoves.add(upLeftTracker);
+                	// add to legal moves as capture move
+                	// legalMoves.add(new Move())
+                	hitPiece = true;
+                } else {
+                	hitPiece = true;
+                }
+            }
         }
 
         // Return an unmodifiable collection containing the legal moves list
         return Collections.unmodifiableList(legalMoves);
     }
-
-
-    // Method to handle edge case for determining illegal moves when the knight is on the "a" (leftmost) column of the
-    // board
-    private static boolean isFirstColumnException(final int currentLocation, final int potentialMoveOffset) {
-        // If the knight is in the first column, it cannot make any of the moves that move it to the left
-        return GameUtilities.COLUMN_A[currentLocation] && ((potentialMoveOffset == -17) || (potentialMoveOffset == -10)
-                || (potentialMoveOffset == 6) || (potentialMoveOffset == 15 ));
-    }
-
-    // Method to handle edge case for determining illegal moves when the knight is on the "h" (rightmost) column of the
-    // board
-    private static boolean isLastColumnException(final int currentLocation, final int potentialMoveOffset) {
-        // If the knight is in the last column, it cannot make any of the moves that move it to the right
-        return GameUtilities.COLUMN_H[currentLocation] && ((potentialMoveOffset == -15) || (potentialMoveOffset == -6)
-                || (potentialMoveOffset == 10) || (potentialMoveOffset == 17));
-    }
-
-    // Method to handle edge case for determining illegal moves when the knight is on colun B (second from left) of the
-    // board
-    private static boolean isColumnBException(final int currentLocation, final int potentialMoveOffset) {
-        // If the knight is in the second columns, it cannot make any of the moves that move two columns to the left
-        return GameUtilities.COLUMN_B[currentLocation] && ((potentialMoveOffset == 6) || (potentialMoveOffset == -10)));
-    }
-
-    // Method to handle edge case for determining illegal moves when the knight is on column G (second from right) of the
-    // board
-    private static boolean isColumnGException(final int currentLocation, final int potentialMoveOffset) {
-        // If the knight is in the Seventh columns, it cannot make any of the moves that move two columns to the right
-        return GameUtilities.COLUMN_G[currentLocation] && ((potentialMoveOffset == -6) || (potentialMoveOffset == 10));
-    }
 }
+
+// Write general method for checking and adding moves in all four directions
+// Something like private void getDirectionMoves(List legalMoves, int positionTracker, int incramentValue, char edgeColumn)
