b0756fd51ee8c6c4d5274f03842abd5268ca13a9
jmac522 <familyguy5115@gmail.com>
2018-10-18 19:26:17 +0000

    additional commenting

--- src/BlackPlayer.java
+++ src/BlackPlayer.java
@@ -1,21 +1,27 @@
 import java.util.Collection;
 
 public class BlackPlayer extends Player {
+	
+	// Constructor for the Black Player
     public BlackPlayer(Board board, Collection<Move> whiteInitialLegalMoves, Collection<Move> blackInitialLegalMoves) {
 
         super(board, blackInitialLegalMoves, whiteInitialLegalMoves);
     }
-
+	
+	// Method to return all active black pieces
     @Override
     public Collection<Piece> getActivePieces() {
         return this.board.getBlackPieces();
     }
-
+    
+    
+	// Method to return the Side Enum for the player with the black pieces
     @Override
     public Side getSide() {
         return Side.BLACK;
     }
-
+	
+	// Method to return the opponent of the Player with the black pieces
     @Override
     public Player getOpponent() {
         return this.board.whitePlayer();
--- src/WhitePlayer.java
+++ src/WhitePlayer.java
@@ -1,20 +1,24 @@
 import java.util.Collection;
 
 public class WhitePlayer extends Player {
+	// Constructor for White player object
     public WhitePlayer(Board board, Collection<Move> whiteInitialLegalMoves, Collection<Move> blackInitialLegalMoves) {
         super(board, whiteInitialLegalMoves, blackInitialLegalMoves);
     }
-
+	
+	// Method to return the active pieces for the player playing white
     @Override
     public Collection<Piece> getActivePieces() {
         return this.board.getWhitePieces();
     }
-
+	
+	// Method to return the proper Side Enum for a player
     @Override
     public Side getSide() {
         return Side.WHITE;
     }
-
+	
+	// Method to return the opponent for the white player
     @Override
     public Player getOpponent() {
         return this.board.blackPlayer();
