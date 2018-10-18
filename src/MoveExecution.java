96a43a41751a4f8241249c00247b609d16f2962b
jmac522 <familyguy5115@gmail.com>
2018-10-18 23:47:26 +0000

    Commenting MoveExecution

--- src/MoveExecution.java
+++ src/MoveExecution.java
@@ -1,5 +1,8 @@
-// Class to handle executing a selected move and generating the new gameboard
-// based on that move
+// Class to keep track needed data during the processes of deciding whether to executing a move
+// Enum MoveStatus keeps track of if proposed move was successful (DONE),
+// illegal move (ILLEGAL_MOVE) or leaves a player in check (PLAYER_IN_CHECK)
+// so the move is executed (or not) accordingly 
+
 public class MoveExecution {
 	// Member fields for the original boardstate prior to executing the move
 	// the move to be executed
@@ -7,7 +10,8 @@ public class MoveExecution {
     private final Board transitionBoard;
     private final Move move;
     private final MoveStatus moveStatus;
-
+	
+	// Constructor 
     public MoveExecution(final Board transitionBoard,
                          final Move move,
                          final MoveStatus moveStatus) {
@@ -16,6 +20,7 @@ public class MoveExecution {
         this.moveStatus = moveStatus;
     }
     
+    // Getter to access a Move's status enum
     public MoveStatus getMoveStatus() {
     	return this.moveStatus;
     }
