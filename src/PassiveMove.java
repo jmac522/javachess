1e56d76406730cf9d2ece8fcbd400554b5ce1c56
jmac522 <familyguy5115@gmail.com>
2018-10-18 23:52:52 +0000

    Commenting Passive Move class

--- src/PassiveMove.java
+++ src/PassiveMove.java
@@ -3,21 +3,24 @@
  * 
  * 
  */
+
+// TODO: Find A BETTER NAME IDIOT!
 public final class PassiveMove extends Move {
 	
 	PassiveMove(final Board board, final Piece movingPiece, final int movingTo) {
 		super(board, movingPiece, movingTo);
 	}
 	
+	// Method to execute a passive move (non capturing) by a Knight, Rook, or Bishop
+	
 	@Overrides
 	public Board execute() {
 		
 		final Board.Builder = new Board.Builder();
 		for (final Piece piece : this.board.getCurrentPlayer().getActivePieces()) {
-			// Loop through all of the player making the move's pieces, if a piece
+			// Loop through all of the pieces of the player making the move, if a piece
 			// is not the one being moved, it can be added to the board in its current
-			// position 
-			// TODO: Override Equals method for piece 
+			// position 			
 			if(!this.movingPiece.equals(piece)) {
 				builder.setPiece(piece);
 			}		
